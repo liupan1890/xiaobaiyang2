b4bb3eebf2bba6645c76d05ff2444b49d035e7fb
liupan1890 <67817756+liupan1890@users.noreply.github.com>
2020-11-08 19:43:58 +0800

    Add files via upload

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,5 @@ module.exports = {
     presets: [
         '@vue/cli-plugin-babel/preset'
     ],
-    plugins: [
-        [
-            "import",
-            { libraryName: "ant-design-vue", libraryDirectory: "es", style: true }
-        ]
-    ]
+
 }
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
-// vue.config.js for less-loader@6.0.0
 module.exports = {
+
     css: {
         loaderOptions: {
             less: {
                 lessOptions: {
+
                     modifyVars: {
                         'primary-color': '#df5659',
                         'link-color': '#df5659',
@@ -11,9 +12,20 @@ module.exports = {
                         'item-active-bg': 'rgba(255, 71, 71, 0.1)',
                         'item-hover-bg': '#eeecec',
                     },
+
                     javascriptEnabled: true,
                 },
             },
         },
-    }
+    },
+
+
+    configureWebpack: config => {
+        config.externals = {
+            vue: "Vue",
+            vuex: "Vuex",
+            "ant-design-vue": "antd",
+            axios: "axios"
+        };
+    },
 };
